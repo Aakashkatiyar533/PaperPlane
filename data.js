fcf2183641591ea63a7f4f85019f82ac47c3dafe
Aakashkatiyar533 <Aakashkatiyar533@gmail.com>
2018-05-29 14:30:39 +0530

    Update data.js

--- data.js
+++ data.js
@@ -78,7 +78,7 @@ document.addEventListener("DOMContentLoaded",function() {
     console.log(k,incr); 
     incr+=1;
 		if(incr > quotes.length)
-      {alert('Thank You for visiting, Thoughts are done for the day.');
+      {console.log(hi);alert('Thank You for visiting, Thoughts are done for the day.');
       document.getElementById("generateButton").style.display = 'none';}
 		else
 		newQuote(k);	
